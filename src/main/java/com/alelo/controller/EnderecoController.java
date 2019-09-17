d8df5e2b34f6294723afcf24d8823c698d31cf04
csantos <cris91@hotmail.com.br>
2019-09-17 08:38:43 -0300

    retirando main desnecessario

--- src/main/java/com/alelo/controller/EnderecoController.java
+++ src/main/java/com/alelo/controller/EnderecoController.java
@@ -1,6 +1,5 @@
 package com.alelo.controller;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,11 +15,6 @@ public class EnderecoController {
 
 	RestTemplate restTemplate;
 
-	public static void main(String[] args) {
-		SpringApplication.run(EnderecoController.class,
-				args);
-	}
-	
 	@ApiOperation(value = "Consulta endereco pelo CEP")
 	@GetMapping("/{cep}")
 	public ResponseEntity<String> consultaCep(@PathVariable(value = "cep") String cep) {
